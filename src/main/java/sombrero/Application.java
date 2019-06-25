3862a1e8ca98d201fc2e249b9f7e41b09be3a3c5
sombrero104 <sombrero.khy@gmail.com>
2019-06-25 23:44:33 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -10,4 +10,17 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * Hypermedia As The Engine Of Application State
+     * ● 서버: 현재 리소스와 연관된 링크 정보를 클라이언트에게 제공한다.
+     * ● 클라이언트: 연관된 링크 정보를 바탕으로 리소스에 접근한다.
+     * ● 연관된 링크 정보
+     * ○ Relation
+     * ○ Hypertext Reference)
+     * ● spring-boot-starter-hateoas 의존성 추가
+     * ● https://spring.io/understanding/HATEOAS
+     * ● https://spring.io/guides/gs/rest-hateoas/
+     * ● https://docs.spring.io/spring-hateoas/docs/current/reference/html/
+     */
+
 }

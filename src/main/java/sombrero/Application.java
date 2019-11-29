34fd48497cf17ec42fdf732552b60b1d7e4f3b1e
sombrero104 <sombrero.khy@gmail.com>
2019-11-29 17:36:06 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -11,13 +11,21 @@ public class Application {
     }
 
     /**
-     * Hypermedia As The Engine Of Application State
-     * ● 서버: 현재 리소스와 연관된 링크 정보를 클라이언트에게 제공한다.
-     * ● 클라이언트: 연관된 링크 정보를 바탕으로 리소스에 접근한다.
+     * 스프링의 HATEOAS
+     * HATEOAS를 위해 편리한 기능들을 제공해주는 일종의 스프 라이브러리.
+     *
+     * HATEOAS (Hypermedia As The Engine Of Application State)
+     * ● 서버:
+     *      현재 rest API 리소스와 연관된 링크 정보를 클라이언트에게 제공한다.
+     * ● 클라이언트:
+     *      연관된 링크 정보를 바탕으로 rest API 리소스에 접근한다.
+     *
      * ● 연관된 링크 정보
      * ○ Relation
      * ○ Hypertext Reference)
+     *
      * ● spring-boot-starter-hateoas 의존성 추가
+     *
      * ● https://spring.io/understanding/HATEOAS
      * ● https://spring.io/guides/gs/rest-hateoas/
      * ● https://docs.spring.io/spring-hateoas/docs/current/reference/html/

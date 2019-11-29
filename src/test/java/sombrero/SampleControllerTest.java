6b6015e143e5c6497a7814230d635565e2388644
sombrero104 <sombrero.khy@gmail.com>
2019-11-29 22:31:00 +0900

    .

--- src/test/java/sombrero/SampleControllerTest.java
+++ src/test/java/sombrero/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package sombrero;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,15 @@ public class SampleControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    /**
+     * JSON 변환을 위한 ObjectMapper 사용 시
+     * ObjectMapper가 이미 빈으로 등록되어 있기 때문에
+     * (spring-boot-starter-web 의존성만 추가해도 자동으로 빈으로 등록됨.)
+     * 아래처럼 그냥 주입만 받아서 사용하면 됨.
+     */
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void hello() throws Exception {
